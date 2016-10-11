82522bfbb25e7ed0c1cf20a4ec470af2ee4275c4
Armin Eslami <armin.eslami@gmail.com>
2016-10-10 23:52:43 -0400

    docs: Add js file
    the js file adds the interactivity to the form using checkbox.

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function billingFunction(){
+	if(document.getElementById('same').checked) {
+	var shipName = document.getElementById('shippingName').value;
+	document.getElementById('billingName').value = shipName;
+
+	var shipZip = document.getElementById('shippingZip').value;
+	document.getElementById('billingZip').value = shipZip;
+	}
+	else {
+	document.getElementById('billingName').value = "";
+
+	document.getElementById('billingZip').value = "";
+	}
+}
