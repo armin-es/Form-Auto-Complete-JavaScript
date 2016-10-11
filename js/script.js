f122791ed3bff1e8139e8c17f59e190104c63913
Alireza Saberi <saberi.ar.eng@gmail.com>
2016-10-11 18:55:12 -0400

    docs: jsdoc
    checking jsdoc on the js file
    
    [ticket : #8]

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 function billingFunction(){
+    /**
+    * @description If checkmark is marked, then put the shiping address as same as billing infomation
+    * @param {number} N/A
+    * @param {number} N/A
+    * @returns {number} N/A
+    */
+
     if( document.getElementById( 'same' ).checked ) {
         var shipName = document.getElementById( 'shippingName' ).value;
         document.getElementById( 'billingName' ).value = shipName;
